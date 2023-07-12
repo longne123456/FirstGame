79e44eca0f5699d266de66f07515eda8eaca997e
login106287 <tquanghuy2607@gmail.com>
2023-07-12 21:56:53 +0700

    Thêm class fighter

--- Fighter.py
+++ Fighter.py
@@ -1,1 +1,82 @@
 #Làm nhân vật
+
+import pygame
+
+class Fighter():
+    def __init__(self, player, x, y, flip, data):
+        self.flip = False
+        self.rect = pygame.Rect((x, y, 80, 180))
+        self.vel_y = 0
+        self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
+
+
+    def move(self, screen_width, screen_height, surface, target):
+        SPEED = 10
+        GRAVITY = 2
+        dx = 0
+        dy = 0
+
+        #get keypresses
+        key = pygame.key.get_pressed()
+
+        if self.attacking == False:
+            #movement
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
+            #jump
+            if key[pygame.K_w] and self.jump == False :
+                self.vel_y = -30
+                self.jump = True
+            #attack
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
+                #determine which attack type was used
+                if key[pygame.K_r]:
+                    self.attack_type = 1
+                if key[pygame.K_t]:
+                    self.attack_type = 2
+                pygame.time.delay(150)
+                self.attacking = False
+
+        #apply gravity
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+
+        #ensure player stays on screen
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > screen_width:
+            dx = screen_width - self.rect.right
+        if self.rect.bottom + dy > screen_height - 70:
+            self.vel_y = 0
+            self.jump = False
+            dy = screen_height - 70 - self.rect.bottom
+
+        #ensure player face each other
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
+        #update player position
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def attack(self, surface, target):
+        self.attacking = True
+
+        attacking_rect = pygame.Rect(self.rect.centerx - (2*self.rect.width*self.flip), self.rect.y, 2*self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 2
+        
+        
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+
+
+    def draw(seft, surface):
+        pygame.draw.rect(surface, (255, 0, 0), seft.rect)
--- Main.py
+++ Main.py
@@ -10,6 +10,24 @@ pygame.display.set_caption('The chosen one')
 clock = pygame.time.Clock( )
 
 while True:
+
+    #draw bg
+    draw_bg()
+
+    #show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+
+
+    #move fighter
+    fighter_1.move(SCREEN_WIDTH,SCREEN_HEIGHT,screen,fighter_2)
+
+    #draw fighter
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
